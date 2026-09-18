0898c6d30207c07a7eab2822b478176114672412
laasyagattu <gattu.l@northeastern.edu>
2026-09-18 14:44:19 -0400

    Implement subtract

--- class3_calculator.py
+++ class3_calculator.py
@@ -4,6 +4,8 @@ import argparse
 def add(a, b):
     return a + b
 
+def subtract(a, b):
+    return a - b
 
 def main():
     parser = argparse.ArgumentParser(description="A simple calculator")
@@ -11,14 +13,18 @@ def main():
     parser.add_argument("--b", "-b", type=float, required=True, help="Second number")
     parser.add_argument(
         "--operation", "-op",
-        choices=["add"],
+        choices=["add", "subtract"],
         default="add",
         help="Operation to perform"
     )
+
     args = parser.parse_args()
 
     if args.operation == "add":
         result = add(args.a, args.b)
+    elif args.operation == "subtract":
+        result = subtract(args.a, args.b)
+
 
     print(f"Result: {result}")
 
